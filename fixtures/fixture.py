5823c5ad71e2f0b46afa75e297600753820f5ab1
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2024-05-21 18:34:15 +0300

    python-version: 3.12

--- fixtures/fixture.py
+++ fixtures/fixture.py
@@ -2,18 +2,24 @@ import os.path
 from _pytest.fixtures import fixture
 from selenium import webdriver
 from page_object.main_page import ShadyMeadowsPageObject
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+
+def chrome(debug=False):
+    options = ChromeOptions()
+    options.add_argument('--headless=new')
+    driver = webdriver.Chrome() if debug else \
+        webdriver.Chrome(options)
+    return driver
 
 
 @fixture
 def shady_meadow_page(custom_logger):
-    driver = webdriver.Chrome()
+    driver = chrome()
     driver.maximize_window()
     yield ShadyMeadowsPageObject(driver, custom_logger).open()
 
     driver.quit()
-
-
-
 
     # from selenium import webdriver
     # from selenium.webdriver.chrome.service import Service
